9e21a9a0bb26857418df50ffddb94198cdf48988
Piya308 <priyankagaikwad1185@gmail.com>
2025-07-29 21:45:34 +0530

    Envrionment interface for accessing config value system properties

--- accounts/src/main/java/com/priyanka/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/priyanka/accounts/controller/AccountsController.java
@@ -15,6 +15,7 @@ import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -32,6 +33,9 @@ public class AccountsController {
     @Value("${build.version}")
     private String buildVersion;
 
+    @Autowired
+    private Environment environment;
+
     @Operation(
             summary = "Get build information",
             description = "Http Status Internal Server Error")
@@ -40,6 +44,16 @@ public class AccountsController {
         return
                 ResponseEntity.status(HttpStatus.OK)
                         .body(buildVersion);
+    }
+
+    @Operation(
+            summary = "Get java version information",
+            description = "Http Status Internal Server Error")
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion(){
+        return
+                ResponseEntity.status(HttpStatus.OK)
+                        .body(environment.getProperty("JAVA_HOME"));
     }
 
     @Operation(
